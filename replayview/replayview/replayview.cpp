DankRank/replayview

commit 42e73aac81af5c0a8e96ba0c1c6690b8dfad5008

Made resource strings dynamically allocated
Working with raw resource data is fun.

replayview/replayview/replayview.cpp
@@ -101,15 +101,30 @@ public:
 	const UserChunk* gameInfo;
 	const UserChunk* comment;
 	DWORD fileSize;
-	TCHAR errCaption[256];
-	TCHAR errText[256];
+	TCHAR *errCaption;
+	TCHAR *errText;
 	
 	explicit DialogData(HINSTANCE hInst);
 	int locateSections();
 	bool Save(TCHAR* wcomment, int wlen);
 	void Cleanup();
 	~DialogData();
+private:
+	TCHAR* GetResourceString(UINT sid);
 };
+
+TCHAR* DialogData::GetResourceString(UINT sid) {
+	const TCHAR *str;
+	int len = LoadString(hInstance, sid, (LPTSTR)&str, 0);
+	if (!len) {
+		DebugBreak();
+		return new TCHAR();
+	}
+	TCHAR* rvstr = new TCHAR[len+1];
+	memcpy(rvstr, str, len * sizeof(TCHAR));
+	rvstr[len] = 0;
+	return rvstr;
+}
 
 DialogData::DialogData(HINSTANCE hInst){
 	hInstance = hInst;
@@ -118,12 +133,8 @@ DialogData::DialogData(HINSTANCE hInst){
 	gameInfo = nullptr;
 	comment = nullptr;
 	fileSize = 0;
-	if (!LoadString(hInstance, IDS_ERR_CAPTION, errCaption, ARRAY_SIZE(errCaption))) {
-		DebugBreak();
-	}
-	if (!LoadString(hInstance, IDS_ERR_TEXT, errText, ARRAY_SIZE(errText))) {
-		DebugBreak();
-	}
+	errCaption = GetResourceString(IDS_ERR_CAPTION);
+	errText = GetResourceString(IDS_ERR_TEXT);
 }
 
 int DialogData::locateSections() {
@@ -216,6 +227,8 @@ void DialogData::Cleanup() {
 }
 DialogData::~DialogData() {
 	Cleanup();
+	delete[] errCaption;
+	delete[] errText;
 }
 intptr_t __stdcall DialogFunc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	DialogData *d = reinterpret_cast<DialogData*>( GetWindowLongPtr(hWnd, GWLP_USERDATA) );

commit fde6436012733f18696e2886ea164d393abe5628

Resource strings are now acquired manually.
This fixes a crash on Win98, which doesn't support directly getting
the string from resources using LoadString.

There's aparently no way to get length of a string resource, other than
praying that your buffer is big enough to hold it. There isn't even any
way to know if string fit into the buffer or not. So, I had to access
resources manually to replicated what LoadString allows you to do on WinNT.

The way STRINGTABLE is organized in rsrc is a bit more complicated than
with other resources:
 - Each individual string is a WORD (length), followed by zero or more
   wchar_t. Length is in characters, and doesn't include itself.
 - Strings are grouped in groups of 16, and those groups are stored as
   resources with type RT_STRING.
 - Resource 1 is responsible for strings 0-15. Resource 2 - for 16-31,
   etc.

Regression from 42e73aac81af5c0a8e96ba0c1c6690b8dfad5008

replayview/replayview/replayview.cpp
@@ -105,8 +105,8 @@ public:
 	const UserChunk* gameInfo;
 	const UserChunk* comment;
 	DWORD fileSize;
-	TCHAR *errCaption;
-	TCHAR *errText;
+	wchar_t *errCaption;
+	wchar_t *errText;
 	
 	explicit DialogData(HINSTANCE hInst);
 	int locateSections();
@@ -121,15 +121,35 @@ private:
 	TCHAR* GetResourceString(UINT sid);
 };
 
-TCHAR* DialogData::GetResourceString(UINT sid) {
-	const TCHAR *str;
-	int len = LoadString(hInstance, sid, (LPTSTR)&str, 0);
+size_t LoadStringSafe(HINSTANCE hInst, UINT sid, const wchar_t**outstr, WORD lang = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL)) {
+	*outstr = nullptr;
+
+	HRSRC hResInfo = FindResourceEx(hInst, RT_STRING, MAKEINTRESOURCE(sid / 16 + 1), lang);
+	if (!hResInfo) return 0;
+	HGLOBAL hGlobal = LoadResource(hInst, hResInfo);
+	if (!hGlobal) return 0;
+	wchar_t* str = (wchar_t*)LockResource(hGlobal);
+	if (!str) return 0;
+
+	// Seek to our string
+	for (unsigned i = 0; i<sid % 16; i++) {
+		str += 1 + (WORD)*str;
+	}
+
+	WORD len = (WORD)*str;
+	*outstr = ++str;
+	return len;
+}
+
+wchar_t* DialogData::GetResourceString(UINT sid) {
+	const wchar_t *str;
+	int len = LoadStringSafe(hInstance, sid, &str);
 	if (!len) {
 		DebugBreak();
-		return new TCHAR();
+		return new wchar_t();
 	}
-	TCHAR* rvstr = new TCHAR[len+1];
-	memcpy(rvstr, str, len * sizeof(TCHAR));
+	wchar_t* rvstr = new wchar_t[len+1];
+	memcpy(rvstr, str, len * sizeof(wchar_t));
 	rvstr[len] = 0;
 	return rvstr;
 }